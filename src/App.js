62d044f34cfc44cd15089948a4e9526a1dbb4813
mkpMac <mkp0131@naver.com>
2021-01-30 21:03:32 +0900

    6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import Potato from "./Potato";
+import React from 'react';
+import Button from './Button';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <Potato name="kkkkk" papapa={['🍟','🍓','🥞']}/>
-    </div>
-  );
+class App extends React.PureComponent {
+  state = {
+    isLoading: true,
+    movie: []
+  }
+
+  loadingHandler = () => {
+    this.setState(() => {
+      return {
+        isLoading: false
+      }
+    })
+  }
+
+  getMovieList = async () => {
+    const movieList = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    if(movieList.status === 200) this.loadingHandler();
+    console.log('', movieList);
+  }
+
+  componentDidMount() {
+    this.getMovieList();
+  }
+
+
+  render() {
+    const {isLoading} = this.state;
+    return (
+      <div>
+        <div>
+          {isLoading ? 'Loading....' : 'Ready'}
+        </div>
+        <Button onClick={this.loadingHandler}/>
+      </div>
+    )
+  }
 }
 
 export default App;
